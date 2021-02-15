04dc673de423e5e69a3a04dd37db394b23900fae
Valera Rozuvan <2c7v7czk85j3@protonmail.com>
2021-02-15 13:47:16 +0200

    update site

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7a878dc9287fb9c2dfc4969534645802.js"
+  "/precache-manifest.b2e6428800df9ac0377380fb8e8957bd.js"
 );
 
 self.addEventListener('message', (event) => {
