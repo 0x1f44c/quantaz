a0a625033da07c65f1bb66bffe43f4bd8c35e2b8
DarinaSednevets <dariaausten@gmail.com>
2021-02-17 14:10:10 +0200

    add some html  & styles to landing page

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -4,10 +4,28 @@ import './landing.scss'
 class Landing extends Component {
   render() {
     return (
-      <div className="landing-container">
-        <p>Privacy Guard Messaging</p>
-        <a className="cool-button" href="/msg">Start messaging</a>
+      <div class="bg-for-landing">
+        <div className="container landing-container">
+          <header class="header-container">
+            <div class="landing-logo-div">
+              <p>
+                <a className="link  header-logo-ladning" href="/" aria-label="logo of privacy guard">PG</a>
+              </p>
+              <p class="landing-logo-messaging">Messaging</p>
+            </div>
+            <button className=" menu-btn-landing" type="button" aria-label="menu button">
+              <svg className=" menu-svg" width="50px" height="50px">
+                <use className="icon-menu" href="./sprite.svg#icon-menu-btn"></use>
+              </svg>
+            </button>
+
+          </header>
+          <main class="main-landing-container">
+            <a class="cool-button" href="/msg">message</a>
+          </main>
+        </div>
       </div>
+
     )
   }
 }
