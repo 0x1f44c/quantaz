c9eb5e986be64b3508f6b3415293486f8fc71019
DarinaSednevets <dariaausten@gmail.com>
2021-12-03 13:21:03 +0200

    MessageList on props

--- src/components/Messenger/AddKey/AddKey.js
+++ src/components/Messenger/AddKey/AddKey.js
@@ -14,6 +14,7 @@ function AddKey(props) {
 
     const addKeyClose = () => {
         props.closeMe();
+        reset();
     }
 
     const handleSubmit = event => {
--- src/components/Messenger/MessageList/MessageList.js
+++ src/components/Messenger/MessageList/MessageList.js
@@ -1,42 +1,41 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { useState, useEffect, useRef } from 'react';
 import { Message } from '../Message/Message'
 import { Sender } from '../Sender/Sender'
 import './MessageList.scss'
 
 
-class MessageList extends Component {
-    state = {
-        messages: [],
-    }
-    componentDidMount() {
-        this.scrollToBottom();
-    }
+function MessageList(props) {
 
-    componentDidUpdate() {
-        this.scrollToBottom();
-    }
+    const [messages] = useState([]);
 
-    scrollToBottom = () => {
-        this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+    const messagesEndRef = useRef(null)
+
+    useEffect(() => {
+        scrollToBottom()
+    }, [messages]);
+
+    const scrollToBottom = () => {
+        return messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
     }
 
-    render() {
+    return (
+        <section className="messages-section">
+            <h2 className="messages">messages from {props.activeKey.publicKey}</h2>
+            <ul className="list messages-wrapper">{
+                props.activeKey.messages.map(({ sender, body, key }) => {
+                    return (<Message sender={sender} body={body} key={key} />)
+                })
+            }
+                <div
+                    style={{ float: "left", clear: "both", paddingTop: "1em", paddingBottom: "1em" }}
+                    ref={messagesEndRef}>
+                </div>
+            </ul>
+            <Sender addNewMessage={props.addNewMessage} />
+        </section>
+    )
 
-        return (
-            <section className="messages-section">
-                <h2 className="messages">messages from {this.props.activeKey.publicKey}</h2>
-                <ul className="list messages-wrapper">{
-                    this.props.activeKey.messages.map(({ sender, body, key }) => {
-                        return (<Message sender={sender} body={body} key={key} />)
-                    })
-                }
-                    <div style={{ float: "left", clear: "both", paddingTop: "1em", paddingBottom: "1em" }}
-                        ref={(el) => { this.messagesEnd = el; }}></div>
-                </ul>
-                <Sender addNewMessage={this.props.addNewMessage} />
-            </section>
-
-        )
-    }
 }
 export { MessageList };
+
